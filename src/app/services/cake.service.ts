fc06c0aa1266085ed4a85bdbf3415f2848db9218
mnh <martin.hylleberg@systematic.com>
2017-03-17 11:14:05 +0100

    initial commit

---
+++ src/app/services/cake.service.ts
@@ -0,0 +1,26 @@
+import {Injectable} from "@angular/core";
+import "rxjs/Rx";
+
+@Injectable()
+export class CakeService {
+  private nextCake: Date;
+  // http: any;
+  // patientsUrl: string;
+
+  constructor() {
+    this.nextCake = new Date();
+    this.nextCake.setHours(13);
+    this.nextCake.setMinutes(30);
+    // this.http = http;
+    // this.patientsUrl = "http://localhost:8080/EventArchitecture/patients/";
+  }
+
+  getNextCake() {
+    return this.nextCake;
+  }
+  // getPatients() {
+  //   return this.http.get(this.patientsUrl)
+  //     .map(res => res.json());
+  // }
+
+}
